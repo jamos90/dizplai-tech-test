078ed5549f52a36cc50df6033d478535387e27e8
jamos90 <jamosamos90@gmail.com>
2025-02-08 20:17:50 +0000

    updates styling file in poll option compoent and class in template

--- front-end/src/app/shared/components/poll-option/poll-option.component.ts
+++ front-end/src/app/shared/components/poll-option/poll-option.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'app-poll-option',
   imports: [],
   templateUrl: './poll-option.component.html',
-  styleUrl: './poll-option.component.sass'
+  styleUrl: './poll-option.component.scss'
 })
 export class PollOptionComponent {
   @Input({ required: true }) entryTitle: string | undefined;
