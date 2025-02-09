23ac20bdb64f19288d3e89cad6c1f64f1fa3d025
jamos90 <jamosamos90@gmail.com>
2025-02-09 19:09:01 +0000

    fixes inital bugs with vote adding, needs more testing to find others

--- backend/src/services/polls.service.js
+++ backend/src/services/polls.service.js
@@ -1,12 +1,16 @@
 const dbInstance = require("../db/db-connection");
+const PollModel = require("../models/poll.model");
+const VoteModel = require("../models/vote.model");
+const OptionModel = require("../models/option.model");
 
 class PollService {
   //TODO:: add db logic when we have it set up
 
   async getAllPolls() {
     try {
-      const polls = await dbInstance("polls").select("*");
-      console.log(polls);
+      const polls = PollModel.query()
+        .where("status", "active")
+        .withGraphFetched("options");
       return polls;
     } catch (err) {
       console.error("Error fetch all polls", err);
@@ -16,8 +20,37 @@ class PollService {
       };
     }
   }
+
+  async addVote(optionId, pollId) {
+    let updatedPoll;
+    //BUG need to check if option exits before adding to votes table;
+    try {
+      await VoteModel.transaction(async transaction => {
+        await VoteModel.query(transaction).insert({
+          option_id: optionId,
+          timestamp: new Date()
+        });
+
+        await OptionModel.query(transaction)
+          .where("id", optionId)
+          .increment("totalVotes", 1);
+
+        updatedPoll = await PollModel.query(transaction)
+          .where("id", pollId)
+          .first()
+          .withGraphFetched("options");
+
+        console.log("updated option", updatedPoll);
+      });
+      return updatedPoll;
+    } catch (err) {
+      console.error(`Error adding vote to option with ${optionId}`, err);
+      return false;
+    }
+  }
 }
 
 module.exports = {
-  getAllPolls: PollService.prototype.getAllPolls
+  getAllPolls: PollService.prototype.getAllPolls,
+  addVote: PollService.prototype.addVote
 };
