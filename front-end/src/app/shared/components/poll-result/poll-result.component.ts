e6d8027d149d815352553b9002d7fbc31068c41f
jamos90 <jamosamos90@gmail.com>
2025-02-09 12:29:12 +0000

    updates poll result view with percentage calcualtor elemement - using mock data at the moemtn

--- front-end/src/app/shared/components/poll-result/poll-result.component.ts
+++ front-end/src/app/shared/components/poll-result/poll-result.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-poll-result',
   imports: [ProgressBarComponent],
   templateUrl: './poll-result.component.html',
-  styleUrl: './poll-result.component.sass'
+  styleUrl: './poll-result.component.scss'
 })
-export class PollResultComponent {}
+export class PollResultComponent {
+  @Input() option: any;
+  @Input() totalVotes: number;
+
+  percentageOfTotalVotes: number;
+
+  ngOnInit() {
+    const percentageOfVote = this.calculatePercentageOfVote();
+    this.percentageOfTotalVotes = percentageOfVote;
+  }
+
+  calculatePercentageOfVote() {
+    console.log(Math.round((this.option.voteCount / this.totalVotes) * 100));
+    return Math.round((this.option.voteCount / this.totalVotes) * 100);
+  }
+}
