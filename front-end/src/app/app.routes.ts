ce69fdbe554ad80391ed389a60da7530341f2e38
jamos90 <jamosamos90@gmail.com>
2025-02-09 12:29:57 +0000

    commiting wip stuff

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
 import { VotingViewComponent } from './features/voting-view/voting-view.component';
+import { ResultsViewComponent } from './features/results-view/results-view.component';
 
-export const routes: Routes = [{ path: '', component: VotingViewComponent }];
+export const routes: Routes = [{ path: '', component: ResultsViewComponent }];
