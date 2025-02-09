49f54a4eacc91438c33496c8bddca85ba3c4703c
jamos90 <jamosamos90@gmail.com>
2025-02-09 13:47:19 +0000

    adds a put request to http service and updateVoteCount for polls service

--- front-end/src/app/core/services/http-service/http.service.ts
+++ front-end/src/app/core/services/http-service/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { VoteSubmit } from '../../../models/polls/vote-submit.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class HttpService {
 
   get(endPoint: string): Observable<any> {
     return this.httpClient.get(`${this.apiBase}/${endPoint}`);
+  }
+
+  put(endPoint: string, body: VoteSubmit) {
+    return this.httpClient.put(`${this.apiBase}/${endPoint}`, body);
   }
 }
--- front-end/src/app/core/services/polls-service/polls.service.ts
+++ front-end/src/app/core/services/polls-service/polls.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from '../http-service/http.service';
 import { Observable } from 'rxjs';
+import { VoteSubmit } from '../../../models/polls/vote-submit.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class PollsService {
     const data = this.httpService.get('polls');
     console.log(data);
     return data;
+  }
+
+  updatePollVotesById(requestBody: VoteSubmit): Observable<any> {
+    return this.httpService.put(`polls/${requestBody.pollId}`, requestBody);
   }
 }
