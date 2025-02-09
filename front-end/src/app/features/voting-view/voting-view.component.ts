9a3ec6062d32b2c1a7238a457c7f255bb3de38bc
jamos90 <jamosamos90@gmail.com>
2025-02-09 11:54:14 +0000

    updates voting view for data and styles

--- front-end/src/app/features/voting-view/voting-view.component.ts
+++ front-end/src/app/features/voting-view/voting-view.component.ts
@@ -1,29 +1,24 @@
 import { Component } from '@angular/core';
 import { PollOptionComponent } from '../../shared/components/poll-option/poll-option.component';
 import { PollsService } from '../../core/services/polls-service/polls.service';
+import { CommonModule } from '@angular/common';
+import { DataService } from '../../core/services/data-service/data.service';
 
 @Component({
   selector: 'app-voting-view',
-  imports: [PollOptionComponent],
-  providers: [PollsService],
+  imports: [PollOptionComponent, CommonModule],
+  providers: [PollsService, DataService],
   templateUrl: './voting-view.component.html',
   styleUrl: './voting-view.component.scss'
 })
 export class VotingViewComponent {
-  constructor(private pollService: PollsService) {}
-  poll = {
-    name: 'test-poll',
-    entries: [
-      {
-        name: 'test-entry-1',
-        votes: 0
-      },
-      {
-        name: 'test-entry-2',
-        votes: 0
-      }
-    ]
-  };
+  constructor(
+    private pollService: PollsService,
+    private dataService: DataService
+  ) {}
+  poll;
+  selectedEntry;
+  selectedId;
 
   ngOnInit() {
     this.pollService.getAllPolls().subscribe(
@@ -34,10 +29,17 @@ export class VotingViewComponent {
 
   updateSelectedEntry(index: number) {
     console.log('selected entry', this.poll.entries[index]);
+    this.selectedEntry = this.poll.entries[index];
+    this.selectedId = index;
+    this.updateDataService(this.selectedEntry);
   }
 
   showData(data: any) {
     console.log(data.mockData.polls[0]);
     this.poll = data.mockData.polls[0];
+  }
+
+  updateDataService(data: any) {
+    this.dataService.updateData(this.selectedEntry);
   }
 }
