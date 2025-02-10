648143a19c0eca7f7f7a0619fed9a2a32d383bc4
jamos90 <jamosamos90@gmail.com>
2025-02-10 10:06:49 +0000

    updates validation using joi for body request

--- backend/src/validations/poll.validations.js
+++ backend/src/validations/poll.validations.js
@@ -4,10 +4,27 @@ const schema = Joi.object({
   name: Joi.string()
     .min(1)
     .required(),
+  description: Joi.string()
+    .required()
+    .min(1),
+  status: Joi.string().required(),
+  totalVotes: Joi.number()
+    .required()
+    .default(0),
   entries: Joi.array()
     .required()
     .min(2)
     .max(7)
+    .items(
+      Joi.object({
+        name: Joi.string()
+          .min(1)
+          .required(),
+        totalVotes: Joi.number()
+          .required()
+          .default(0)
+      })
+    )
 });
 
 //TODO: add in validation for duplicate values;
