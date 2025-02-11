98a50dfb313aac082f0aaec2480bfa336d2360f6
jamos90 <jamosamos90@gmail.com>
2025-02-11 12:11:17 +0000

    adds first tests for poll serivce

--- front-end/src/app/core/services/polls-service/polls.service.spec.ts
+++ front-end/src/app/core/services/polls-service/polls.service.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
-
 import { PollsService } from './polls.service';
+import { HttpService } from '../http-service/http.service';
+import { of, throwError } from 'rxjs';
 
 describe('PollsService', () => {
   let service: PollsService;
+  let httpServiceSpy: jasmine.SpyObj<HttpService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const spy = jasmine.createSpyObj('HttpService', ['get', 'post']);
+
+    TestBed.configureTestingModule({
+      providers: [PollsService, { provide: HttpService, useValue: spy }]
+    });
     service = TestBed.inject(PollsService);
+    httpServiceSpy = TestBed.inject(HttpService) as jasmine.SpyObj<HttpService>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get data from http service', () => {
+    const mockPoll = {
+      id: 1,
+      name: 'test',
+      description: 'est-poll',
+      totalVotes: 0,
+      options: []
+    };
+    httpServiceSpy.get.and.returnValue(of(mockPoll));
+
+    service.getAllPolls().subscribe(data => expect(data).toEqual(mockPoll));
+    expect(httpServiceSpy.get).toHaveBeenCalled();
+  });
+
+  it('should post data to http service put request', () => {
+    const mockPoll = {
+      id: 1,
+      name: 'test',
+      description: 'est-poll',
+      totalVotes: 0,
+      options: []
+    };
+    httpServiceSpy.put.and.returnValue(of(mockPoll));
+
+    service.updatePollVotesById({ optionId: 1, pollId: 1 }).subscribe();
+    // expect(httpServiceSpy.get).toHaveBeenCalledOnceWith({});
   });
 });
