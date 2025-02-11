c1ee3633e359be8b4b7faa6e62d8ab102c22b973
jamos90 <jamosamos90@gmail.com>
2025-02-11 18:01:50 +0000

    adds defualt message for poll option and test

--- front-end/src/app/shared/components/poll-option/poll-option.component.spec.ts
+++ front-end/src/app/shared/components/poll-option/poll-option.component.spec.ts
@@ -29,6 +29,14 @@ describe('PollOptionComponent', () => {
     expect(title.textContent).toBe('Test');
   });
 
+  it('should display default message if no title is passed', () => {
+    component.entryTitle = undefined;
+    fixture.detectChanges();
+    const title = fixture.debugElement.query(By.css('.entry-name'))
+      .nativeElement;
+    expect(title.textContent).toBe('no title found');
+  });
+
   it('should emit an event countVote', () => {
     spyOn(component.countVote, 'emit');
     const voteElement = fixture.debugElement.query(
